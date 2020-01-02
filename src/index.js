dacf02c4f296931eecf6534293116c310fa5f4ad
Henry Netzer <hnetzer19@gmail.com>
2020-01-01 16:53:51 -0800

    all seed data is added, started on node

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 import models, { sequelize } from './models';
 import {
   seedJurisdictions,
@@ -15,6 +17,24 @@ console.log('Starting up application...')
 
 const { Jurisdiction, Company, CompanyJurisdiction, Agency, Filing } = models;
 
+const countSeeds = () => {
+  Jurisdiction.count().then(c => {
+    console.log("There are " + c + " jurisdictions!")
+  })
+  Company.count().then(c => {
+    console.log("There are " + c + " companies!")
+  })
+  CompanyJurisdiction.count().then(c => {
+    console.log("There are " + c + " companies jurisdictions!")
+  })
+  Agency.count().then(c => {
+    console.log("There are " + c + " agencies!")
+  })
+  Filing.count().then(c => {
+    console.log("There are " + c + " filings!")
+  })
+}
+
 sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
   if (eraseDatabaseOnSync) {
     await seedJurisdictions();
@@ -22,25 +42,17 @@ sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
     await seedCompanyJurisdictions();
     await seedAgencies();
     await seedFilings();
+    countSeeds();
   }
-
-  Jurisdiction.count().then(c => {
-    console.log("There are " + c + " jurisdictions!")
-  })
-
-  Company.count().then(c => {
-    console.log("There are " + c + " companies!")
-  })
-
-  CompanyJurisdiction.count().then(c => {
-    console.log("There are " + c + " companies jurisdictions!")
-  })
-
-  Agency.count().then(c => {
-    console.log("There are " + c + " agencies!")
-  })
-
-  Filing.count().then(c => {
-    console.log("There are " + c + " filings!")
-  })
 });
+
+const app = express();
+
+app.use(cors());
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
--- src/seeds.js
+++ src/seeds.js
@@ -81,7 +81,7 @@ const seedAgencies = async () => {
   await createAgency('internal revenue service', 'Federal')
   await createAgency('secretary of state', 'Delaware')
   await createAgency('franchise tax board', 'California')
-  await createAgency('internal revenue service', 'California')
+  await createAgency('secretary of state', 'California')
   await createAgency('assessor', 'San Francisco County')
   await createAgency('tax and treasurer', 'San Francisco')
   await createAgency('assessor', 'Los Angeles County')
@@ -127,12 +127,214 @@ const seedFilings = async () => {
   }, 'internal revenue service', 'Federal')
 
   await createFiling({
-    name: 'form 3921',
-    dueDate: '2020-01-31',
+    name: 'form 1120-w',
+    dueDate: null,
     dueDateYearEndOffsetMonths: 3.5,
     dueDateRegOffestMonths: null,
   }, 'internal revenue service', 'Federal')
 
+  await createFiling({
+    name: 'form 1120-w',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 5.5,
+    dueDateRegOffestMonths: null,
+  }, 'internal revenue service', 'Federal')
+
+  await createFiling({
+    name: 'form 1120-w',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 8.5,
+    dueDateRegOffestMonths: null,
+  }, 'internal revenue service', 'Federal')
+
+  await createFiling({
+    name: 'form 1120-w',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 11.5,
+    dueDateRegOffestMonths: null,
+  }, 'internal revenue service', 'Federal')
+
+  await createFiling({
+    name: 'form 114',
+    dueDate: '2020-04-15',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'internal revenue service', 'Federal')
+
+  await createFiling({
+    name: 'annual report',
+    dueDate: '2020-03-01',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'secretary of state', 'Delaware')
+
+  await createFiling({
+    name: 'statement of information',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: 12,
+  }, 'secretary of state', 'California')
+
+  await createFiling({
+    name: 'form 100',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 3.5,
+    dueDateRegOffestMonths: null,
+  }, 'franchise tax board', 'California')
+
+  await createFiling({
+    name: 'form 100-ES',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 5.5,
+    dueDateRegOffestMonths: null,
+  }, 'franchise tax board', 'California')
+
+  await createFiling({
+    name: 'form 100-ES',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 8.5,
+    dueDateRegOffestMonths: null,
+  }, 'franchise tax board', 'California')
+
+  await createFiling({
+    name: 'form 571-L',
+    dueDate: '2020-05-01',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'assessor', 'San Francisco County')
+
+  await createFiling({
+    name: 'Business License',
+    dueDate: '2020-05-31',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'tax and treasurer', 'San Francisco')
+
+  await createFiling({
+    name: 'Payroll Tax',
+    dueDate: '2020-02-28',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'tax and treasurer', 'San Francisco')
+
+  await createFiling({
+    name: 'Payroll Tax Estimate',
+    dueDate: '2020-04-30',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'tax and treasurer', 'San Francisco')
+
+  await createFiling({
+    name: 'Payroll Tax Estimate',
+    dueDate: '2020-07-31',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'tax and treasurer', 'San Francisco')
+
+  await createFiling({
+    name: 'Payroll Tax Estimate',
+    dueDate: '2020-10-31',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'tax and treasurer', 'San Francisco')
+
+  await createFiling({
+    name: 'franchise tax estimate',
+    dueDate: '2020-06-01',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'secretary of state', 'Delaware')
+
+  await createFiling({
+    name: 'franchise tax estimate',
+    dueDate: '2020-09-01',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'secretary of state', 'Delaware')
+
+  await createFiling({
+    name: 'franchise tax estimate',
+    dueDate: '2020-12-01',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'secretary of state', 'Delaware')
+
+  await createFiling({
+    name: 'form 571-L',
+    dueDate: '2020-05-01',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'assessor', 'Los Angeles County')
+
+  await createFiling({
+    name: 'business license',
+    dueDate: '2020-02-28',
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: null,
+  }, 'office of finance', 'Los Angeles')
+
+  await createFiling({
+    name: 'biennial statement',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: null,
+    dueDateRegOffestMonths: 24,
+  }, 'secretary of state', 'New York')
+
+  await createFiling({
+    name: 'NYC-3',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 3.5,
+    dueDateRegOffestMonths: null,
+  }, 'department of finance', 'New York City')
+
+  await createFiling({
+    name: 'mandatory first installment',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 2.5,
+    dueDateRegOffestMonths: 0,
+  }, 'department of taxation and finance', 'New York')
+
+  await createFiling({
+    name: 'CT-3 extension',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 3.5,
+    dueDateRegOffestMonths: 0,
+  }, 'department of taxation and finance', 'New York')
+
+  await createFiling({
+    name: 'CT-3',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 3.5,
+    dueDateRegOffestMonths: 0,
+  }, 'department of taxation and finance', 'New York')
+
+  await createFiling({
+    name: 'NYC-3 estimate',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 3.5,
+    dueDateRegOffestMonths: 0,
+  }, 'department of finance', 'New York City')
+
+  await createFiling({
+    name: 'CT-400 estimate',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 5.5,
+    dueDateRegOffestMonths: 0,
+  }, 'department of taxation and finance', 'New York')
+
+  await createFiling({
+    name: 'CT-400 estimate',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 8.5,
+    dueDateRegOffestMonths: 0,
+  }, 'department of taxation and finance', 'New York')
+
+  await createFiling({
+    name: 'CT-400 estimate',
+    dueDate: null,
+    dueDateYearEndOffsetMonths: 11.5,
+    dueDateRegOffestMonths: 0,
+  }, 'department of taxation and finance', 'New York')
 }
 
 export {
