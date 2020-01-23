6449ef591a7649a08412dad4e67c3dada827c9cc
Henry Netzer <hnetzer19@gmail.com>
2020-01-22 19:04:49 -0800

    now creating company juridictions with offices

--- src/controllers/companyController.js
+++ src/controllers/companyController.js
@@ -37,27 +37,71 @@ const updateCompany = async (req, res, next) => {
   return res.status(200).json(c)
 }
 
-// TODO: Create the jurisdictions in the function to for the offices;
 // TODO: Check if th user owns the company / offices
 const updateOffices = async (req, res, next) => {
   const companyId = req.params.companyId;
-  const offices = req.body.offices;
+  const companyOffices = req.body.offices;
   const company = await Company.findOne({ where: { id: companyId } });
 
-  // TODO: Delete all of the existing offices first
+
   try {
-   await Office.bulkCreate(offices.map(office => {
+   // Delete all of the existing offices first
+   await Office.destroy({ where: { company_id: companyId } })
+
+  // Create the new offices
+   await Office.bulkCreate(companyOffices.map(office => {
      office.company_id = companyId;
      return office
    }))
- } catch(err) {
-   console.log('Error creating offices')
-   console.log(err)
-   return res.status(500).json({})
- }
 
-  const o = await Office.findAll({ where: { company_id: companyId }, raw: true });
-  return res.status(200).json(o)
+   // Get our newly created offices
+   const offices = await Office.findAll({ where: { company_id: companyId }, raw: true });
+
+   // Figure out which jurisdictions to create
+   const companyJurisdictions = await getCompanyJurisdictions(companyId, offices)
+
+   // Create the jurisdictions
+   await CompanyJurisdiction.bulkCreate(companyJurisdictions);
+
+   return res.status(200).json(offices)
+
+ } catch(err) {
+   console.log(err)
+   return res.status(500).send()
+ }
+}
+
+
+// Helper function to map office locations -> jurisdictions;
+const getCompanyJurisdictions = async (companyId, offices) => {
+  const jurisdictions = await Jurisdiction.findAll({ raw: true });
+  const jurisdictionMap = jurisdictions.reduce((acc, j) => {
+    acc[j.name.toLowerCase()] = j.id;
+    return acc
+  }, {})
+
+  const cjs = []
+  offices.forEach(office => {
+    if (office.city.toLowerCase().trim() === 'san francisco') {
+      cjs.push({ companyId: companyId, jurisdictionId: jurisdictionMap['san francisco']})
+      cjs.push({ companyId: companyId, jurisdictionId: jurisdictionMap['san francisco county']})
+      cjs.push({ companyId: companyId, jurisdictionId: jurisdictionMap['california']})
+    }
+
+    if (office.city.toLowerCase().trim() === 'los angeles') {
+      cjs.push({ companyId: companyId, jurisdictionId: jurisdictionMap['los angeles']})
+      cjs.push({ companyId: companyId, jurisdictionId: jurisdictionMap['los angeles county']})
+      cjs.push({ companyId: companyId, jurisdictionId: jurisdictionMap['california']})
+    }
+
+    if (office.city.toLowerCase().trim() === 'new york') {
+      cjs.push({ companyId: companyId, jurisdictionId: jurisdictionMap['new york city']})
+      cjs.push({ companyId: companyId, jurisdictionId: jurisdictionMap['new york']})
+    }
+  });
+
+  cjs.push({ companyId: companyId, jurisdictionId: jurisdictionMap['federal']})
+  return cjs;
 }
 
 export {
