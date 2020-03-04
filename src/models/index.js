e5a4ef809097483644c59fb8a1caac89b5fd37be
Henry Netzer <hnetzer19@gmail.com>
2020-03-03 22:45:26 -0800

    fixing where to find model config from index file

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,7 @@ import path from 'path';
 // read the sequelize config file
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require('../config/config.json')[env];
 
 let sequelize = null;
 
